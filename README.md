837d2e3b03c328ec7535c860e816cec3ae527830
iPc <syhyz1990@qq.com>
2020-03-02 05:21:59 -0600

    v1.0.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# Mactype助手
+![TamperMonkey v4.9](https://img.shields.io/badge/TamperMonkey-v4.8-brightgreen.svg) ![Chrome x64 v80](https://img.shields.io/badge/Chrome%20x64-v80.0-brightgreen.svg) ![Safari v12](https://img.shields.io/badge/Safari%20-v12.0-brightgreen.svg)
+
+## 💽 安装地址
+[安装地址](	https://www.baiduyun.wiki/install-mactype.html)
+
+## 🔧 插件功能
+- 配合 Mactype 实现Chrome，Edge等不支持关闭 DirectWrite 浏览器的字体渲染支持。
+
+## 🎨 脚本效果
+![2.gif](https://i.loli.net/2020/03/02/wkbXns48HVYEPKy.gif)
+               
+## 📖 使用教程
+[点击查看](http://mp.weixin.qq.com/s?__biz=MzAwOTk3Mjg4Mg==&mid=100001190&idx=1&sn=7cb4356f33b0c5c6a5401d624139e161&chksm=1b56385b2c21b14db6e22c6b803762f7f5ca881dd576c96e506341c3ac95fafffc071db99df5#rd)
+
+
+
---
+++ mactype.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name              Mactype助手
+// @namespace         https://github.com/syhyz1990/mactype
+// @version           1.0.0
+// @icon              https://www.baiduyun.wiki/mactype.png
+// @description       配合 Mactype 实现Chrome，Edge等不支持关闭 DirectWrite 浏览器的字体渲染支持
+// @author            syhyz1990
+// @license           MIT
+// @supportURL        https://github.com/syhyz1990/mactype
+// @updateURL         https://www.baiduyun.wiki/mactype.user.js
+// @downloadURL       https://www.baiduyun.wiki/mactype.user.js
+// @include           *
+// @run-at            document-start
+// @grant             GM_addStyle
+// ==/UserScript==
+
+(function () {
+  'use strict'
+  GM_addStyle(`
+    body,input,textarea {
+        text-shadow: 0px 0px 1px #ACACAC;
+    }
+`)
+})()
