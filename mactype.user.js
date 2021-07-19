127de01e48ad9b34246f98db4f6e609145d90344
iPc <syhyz1990@qq.com>
2021-07-19 11:59:03 +0800

    2.1.7

--- mactype.user.js
+++ mactype.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              Mactype助手
 // @namespace         https://github.com/syhyz1990/mactype
-// @version           2.1.3
+// @version           2.1.7
 // @icon              https://www.baiduyun.wiki/mactype.png
 // @description       Windows下的浏览器浏览网页时文字往往发虚，颜色很淡，看不清楚。有了它可以让浏览器中显示的文字更加清晰，支持Chrome ，360 ，QQ ，Firfox ，Edge  等浏览器。
 // @author            YouXiaoHou
@@ -14,7 +14,6 @@
 // @updateURL         https://www.baiduyun.wiki/mactype.user.js
 // @downloadURL       https://www.baiduyun.wiki/mactype.user.js
 // @match             *://*/*
-// @noframes
 // @run-at            document-start
 // @grant             GM_getValue
 // @grant             GM_setValue
@@ -108,11 +107,11 @@
                 showCloseButton: true,
                 inputLabel: '拖动滑块观察变化，数字越大字越清晰',
                 customClass,
-                footer: '<div style="text-align: center;font-size: 1em">点击查看 <a href="https://www.baiduyun.wiki/tool/install-mactype.html" target="_blank">使用说明</a>，配合 <a href="https://www.baiduyun.wiki/tool/install-mactype.html#增强显示" target="_blank">XHei字体</a> 更清晰，<a href="https://www.baiduyun.wiki/mactype.user.js">检查更新</a><svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="14" height="14"><path d="M445.956 138.812L240.916 493.9c-11.329 19.528-12.066 44.214 0 65.123 12.067 20.909 33.898 32.607 56.465 32.607h89.716v275.044c0 31.963 25.976 57.938 57.938 57.938h134.022c32.055 0 57.938-25.975 57.938-57.938V591.63h83.453c24.685 0 48.634-12.803 61.806-35.739 13.172-22.844 12.343-50.016 0-71.386l-199.42-345.693c-13.633-23.58-39.24-39.516-68.44-39.516-29.198 0-54.897 15.935-68.438 39.516z" fill="#d81e06"/></svg></div>',
+                footer: '<div style="text-align: center;font-size: 1em">点击查看 <a href="https://www.baiduyun.wiki/tool/install-mactype.html" target="_blank">使用说明</a>，配合 <a href="https://www.baiduyun.wiki/tool/install-mactype.html#增强显示" target="_blank">XHei字体</a> 更清晰，<a href="https://www.baiduyun.wiki/tool/install-mactype.html">检查更新</a><svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="14" height="14"><path d="M445.956 138.812L240.916 493.9c-11.329 19.528-12.066 44.214 0 65.123 12.067 20.909 33.898 32.607 56.465 32.607h89.716v275.044c0 31.963 25.976 57.938 57.938 57.938h134.022c32.055 0 57.938-25.975 57.938-57.938V591.63h83.453c24.685 0 48.634-12.803 61.806-35.739 13.172-22.844 12.343-50.016 0-71.386l-199.42-345.693c-13.633-23.58-39.24-39.516-68.44-39.516-29.198 0-54.897 15.935-68.438 39.516z" fill="#d81e06"/></svg></div>',
                 inputAttributes: {
                     min: 0,
-                    max: 1.5,
-                    step: 0.1
+                    max: 1,
+                    step: 0.05
                 },
                 inputValue: util.getValue('current_val')
             }).then((res) => {
@@ -163,19 +162,28 @@
 
         addPluginStyle() {
             let val = util.getValue('current_val');
-            let style =  `
+            let style = `
                 .mactype-popup { font-size: 14px!important }
                 *:not(pre) { -webkit-text-stroke: ${val}px !important; text-stroke: ${val}px !important }
                 ::selection { color: #fff;background: #338fff }
             `;
             util.addStyle('swal-pub-style', 'style', GM_getResourceText('swalStyle'));
             util.addStyle('mactype-style', 'style', style);
+
+            window.onload = () => {
+                util.addStyle('swal-pub-style', 'style', GM_getResourceText('swalStyle'));
+                util.addStyle('mactype-style', 'style', style);
+            };
+        },
+
+        isTopWindow() {
+            return window.self === window.top;
         },
 
         init() {
             this.initValue();
-            !util.getValue('has_init') && this.showSetting();
-            this.registerMenuCommand();
+            this.isTopWindow() && !util.getValue('has_init') && this.showSetting();
+            this.isTopWindow() && this.registerMenuCommand();
             if (util.getValue('white_list').includes(location.host)) {
                 return;
             }
