a4b3ee395df1044bb8f077a99a672d89c830a641
iPc <5344173+syhyz1990@users.noreply.github.com>
2021-12-21 12:02:28 +0800

    2.1.8
    Change CDN to unpkg.com

--- mactype.user.js
+++ mactype.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name              Mactype助手
 // @namespace         https://github.com/syhyz1990/mactype
-// @version           2.1.7
-// @icon              https://www.baiduyun.wiki/mactype.png
+// @version           2.1.8
+// @icon              https://www.youxiaohou.com/mactype.png
 // @description       Windows下的浏览器浏览网页时文字往往发虚，颜色很淡，看不清楚。有了它可以让浏览器中显示的文字更加清晰，支持Chrome ，360 ，QQ ，Firfox ，Edge  等浏览器。
 // @author            YouXiaoHou
 // @license           MIT
-// @homepage          https://www.baiduyun.wiki/tool/install-mactype.html
+// @homepage          https://www.youxiaohou.com/tool/install-mactype.html
 // @supportURL        https://github.com/syhyz1990/mactype
-// @require           https://cdn.jsdelivr.net/npm/sweetalert2@10.15.6/dist/sweetalert2.min.js
-// @resource          swalStyle https://cdn.jsdelivr.net/npm/sweetalert2@10.15.6/dist/sweetalert2.min.css
-// @updateURL         https://www.baiduyun.wiki/mactype.user.js
-// @downloadURL       https://www.baiduyun.wiki/mactype.user.js
+// @require           https://unpkg.com/sweetalert2@10.15.6/dist/sweetalert2.min.js
+// @resource          swalStyle https://unpkg.com/sweetalert2@10.15.6/dist/sweetalert2.min.css
+// @updateURL         https://www.youxiaohou.com/mactype.user.js
+// @downloadURL       https://www.youxiaohou.com/mactype.user.js
 // @match             *://*/*
 // @run-at            document-start
 // @grant             GM_getValue
@@ -106,7 +106,7 @@
                 showCloseButton: true,
                 inputLabel: '拖动滑块观察变化，数字越大字越清晰',
                 customClass,
-                footer: '<div style="text-align: center;font-size: 1em">点击查看 <a href="https://www.baiduyun.wiki/tool/install-mactype.html" target="_blank">使用说明</a>，配合 <a href="https://www.baiduyun.wiki/tool/install-mactype.html#增强显示" target="_blank">XHei字体</a> 更清晰，<a href="https://www.baiduyun.wiki/tool/install-mactype.html">检查更新</a><svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="14" height="14"><path d="M445.956 138.812L240.916 493.9c-11.329 19.528-12.066 44.214 0 65.123 12.067 20.909 33.898 32.607 56.465 32.607h89.716v275.044c0 31.963 25.976 57.938 57.938 57.938h134.022c32.055 0 57.938-25.975 57.938-57.938V591.63h83.453c24.685 0 48.634-12.803 61.806-35.739 13.172-22.844 12.343-50.016 0-71.386l-199.42-345.693c-13.633-23.58-39.24-39.516-68.44-39.516-29.198 0-54.897 15.935-68.438 39.516z" fill="#d81e06"/></svg></div>',
+                footer: '<div style="text-align: center;font-size: 1em">点击查看 <a href="https://www.youxiaohou.com/tool/install-mactype.html" target="_blank">使用说明</a>，配合 <a href="https://www.youxiaohou.com/tool/install-mactype.html#增强显示" target="_blank">XHei字体</a> 更清晰，<a href="https://www.youxiaohou.com/tool/install-mactype.html">检查更新</a><svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="14" height="14"><path d="M445.956 138.812L240.916 493.9c-11.329 19.528-12.066 44.214 0 65.123 12.067 20.909 33.898 32.607 56.465 32.607h89.716v275.044c0 31.963 25.976 57.938 57.938 57.938h134.022c32.055 0 57.938-25.975 57.938-57.938V591.63h83.453c24.685 0 48.634-12.803 61.806-35.739 13.172-22.844 12.343-50.016 0-71.386l-199.42-345.693c-13.633-23.58-39.24-39.516-68.44-39.516-29.198 0-54.897 15.935-68.438 39.516z" fill="#d81e06"/></svg></div>',
                 inputAttributes: {
                     min: 0,
                     max: 1,
